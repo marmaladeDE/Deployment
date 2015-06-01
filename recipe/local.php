97c7c84b7b3655394e0fa3adad42f2f0d2d47144
Stefan Krenz <krenz@marmalade.de>
2015-06-01 19:33:28 +0200

    Improved deployment for local VM

--- recipe/local.php
+++ recipe/local.php
@@ -7,14 +7,16 @@ env('sharedFolder', function() {return '/media/project';});
 task("deploy:local:init:webroot", function() {
     $tempDir = env('deploy_path') . '/tmp';
     $sharedDir = env('sharedFolder');
+    run("umask 0002");
     // Create tmp/ directory if it does not exist
-    run("if [ ! -d {$tempDir} ]; then mkdir {$tempDir}; fi");
+    run("if [ -d {$tempDir} ]; then rm -rf {$tempDir}; fi");
     // Move files in webroot to tmp/ directory
-    run("mv {{deploy_path}}/web/* {{deploy_path}}/tmp ");
+    run("if [ $(find {{deploy_path}}/web/ | wc -l) -gt 1 ]; then mv {{deploy_path}}/web {$tempDir}; fi");
     // Extract web.zip in webroot
     run("unzip {$sharedDir}/archives/web.zip -d {{deploy_path}}/web");
     // Move files from tmp/ directory to webroot, overwriting existing files
-    run("mv -f {{deploy_path}}/tmp/* {{deploy_path}}/web");
+    run("if [ $(find {{deploy_path}}/tmp/ | wc -l) -gt 1 ]; then cp -rf {$tempDir}/* {{deploy_path}}/web; fi");
+    run("chmod -R ug+rwX {{deploy_path}}/web");
 })->desc("Initialize webroot")->setPrivate();
 
 task("deploy:local:db:create", function() {
@@ -28,13 +30,13 @@ task("deploy:local:db:create", function() {
 })->desc("Creating database")->setPrivate();
 
 task("deploy:local:db:import", function() {
-    cd(env('sharedFolder'));
+    cd(env('sharedFolder') . "/archives/");
     $databaseConfiguration = env('db');
     if (is_array($databaseConfiguration)) {
         foreach ($databaseConfiguration as $dbName => $dbCredential) {
-            run("gunzip < {$dbName}.sql.gz | mysql -u {$dbCredential['user']} -p{$dbCredential['password']} -s {$dbName} 2> /dev/null");
+            run("gzip -dc {$dbName}.sql.gz | mysql -u {$dbCredential['user']} -p{$dbCredential['password']} -s {$dbName} 2> /dev/null");
         }
     }
 })->desc("Importing sql")->setPrivate();
 
-task("deploy:local:init", ["deploy:local:init:webroot","deploy:local:db:create"])->desc("Initialize local environment");
+task("deploy:local:init", ["deploy:local:init:webroot","deploy:local:db:create", "deploy:local:db:import"])->desc("Initialize local environment");
